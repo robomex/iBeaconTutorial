1d505625649cd5ae1e35b1045a74420e105cee24
robomex <atmorales@gmail.com>
2014-07-22 23:05:19 -0500

    background ibeacon scanning functionality

--- iBeaconTutorial/AppDelegate.swift
+++ iBeaconTutorial/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     var window: UIWindow?
     var locationManager: CLLocationManager?
 
+    var lastProximity: CLProximity?
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         
@@ -26,6 +27,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         locationManager = CLLocationManager()
         if(locationManager!.respondsToSelector("requestAlwaysAuthorization")) {
             locationManager!.requestAlwaysAuthorization()
+        }
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        locationManager!.startMonitoringForRegion(beaconRegion)
+        locationManager!.startRangingBeaconsInRegion(beaconRegion)
+        locationManager!.startUpdatingLocation()
+        
+        if(application.respondsToSelector("registerUserNotificationSettings:")) {
+            application.registerUserNotificationSettings(
+                UIUserNotificationSettings( forTypes: UIUserNotificationType.Alert | UIUserNotificationType.Sound, categories: nil)
+            )
         }
         
         return true
@@ -56,3 +68,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
 }
 
+extension AppDelegate: CLLocationManagerDelegate {
+    func sendLocalNotificationWithMessage(message: String!) {
+        let notification:UILocalNotification = UILocalNotification()
+        notification.alertBody = message
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        NSLog("didRangeBeacons")
+        var message:String = ""
+        
+        if(beacons.count > 0) {
+            let nearestBeacon:CLBeacon = beacons[0] as CLBeacon
+            
+            if(nearestBeacon.proximity == lastProximity || nearestBeacon.proximity == CLProximity.Unknown) {
+                return
+            }
+            lastProximity = nearestBeacon.proximity
+            
+            switch nearestBeacon.proximity {
+            case CLProximity.Far:
+                message = "You are far away from the beacon"
+            case CLProximity.Near:
+                message = "You are near the beacon"
+            case CLProximity.Immediate:
+                message = "You are in the immediate proximity of the beacon"
+            case CLProximity.Unknown:
+                return
+            }
+        } else {
+            message = "No beacons are nearby"
+        }
+        
+        NSLog("%@", message)
+        sendLocalNotificationWithMessage(message)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        manager.startRangingBeaconsInRegion(region as CLBeaconRegion)
+        manager.startUpdatingLocation()
+        
+        NSLog("You entered the region")
+        sendLocalNotificationWithMessage("You entered the region")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        manager.stopRangingBeaconsInRegion(region as CLBeaconRegion)
+        manager.stopUpdatingLocation()
+        
+        NSLog("You exited the region")
+        sendLocalNotificationWithMessage("You exited the region")
+    }
+}
